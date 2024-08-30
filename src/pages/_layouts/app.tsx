008a0dfc0d66058cb862693395773ae187f7c12a
Laize Ferraz <laizeferraz@gmail.com>
2024-08-30 19:01:47 +1000

    add header & mode toggle

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header'
+
 export function AppLayout() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Dashboard</h1>
-      <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
